f9411176f3fed403a0adf8a6124eeb6179d366c8
aescobarr <agustiescobar@gmail.com>
2022-11-04 12:34:07 +0100

    Transplanted xls support to main branch

--- georef/csv_import.py
+++ georef/csv_import.py
@@ -18,7 +18,7 @@ def check_file_structure(file_array):
         raise EmptyFileException()
     numlinia = 1
     for rows in file_array:
-        if len(rows) != 18:
+        if len(rows) != 16:
             raise NumberOfColumnsException({"numrow": str(numlinia), "numcols": str(len(rows))})
         numlinia = numlinia + 1
 
@@ -127,35 +127,33 @@ def process_line(line, line_string, errors, toponims_exist, toponims_to_create, 
         nom = ''
         # [1] - Tipus toponim --> Buscar toponim per nom TipusToponim
         tt = None
-        # [2] - Pais --> Buscar pais per nom Pais
-        p = None
-        # [3] - Aquatic --> Cap comprovacio
+        # [2] - Aquatic --> Cap comprovacio
         aquatic = 'N'
-        # [4] - Node superior --> Buscar toponim per nom(multiples resultats?)
+        # [3] - Node superior --> Buscar toponim per nom(multiples resultats?)
         pare = None
-        # [5] - Numero de versio
+        # [4] - Numero de versio
         numeroVersio = -1
-        # [6] - Qualificador de la versió
+        # [5] - Qualificador de la versió
         qv = None
-        # [7] - Versio capturada del recurs RecursGeoreferenciacio
+        # [6] - Versio capturada del recurs RecursGeoreferenciacio
         rg = None
-        # [8] - Nom del toponim al recurs
+        # [7] - Nom del toponim al recurs
         nomToponimRecurs = None
-        # [9] - Data
+        # [8] - Data
         data = None
-        # [10] - Coord x original
+        # [9] - Coord x original
         coordX_original = None
-        # [11] - Coord y original
+        # [10] - Coord y original
         coordY_original = None
-        # [12] - Coord z
+        # [11] - Coord z
         coordZ_original = None
-        # [13] - Incertesa de coordenada
+        # [12] - Incertesa de coordenada
         precisioH = None
-        # [14] - Incertesa h
+        # [13] - Incertesa h
         precisioZ = None
-        # [15] - Georeferenciador
+        # [14] - Georeferenciador
         georeferenciador = None
-        # [16] - Observacions
+        # [15] - Observacions
         observacions = None
 
         errorsALinia = False
@@ -182,166 +180,155 @@ def process_line(line, line_string, errors, toponims_exist, toponims_to_create, 
 
         if line[2] == '' or line[2].strip() == '':
             errorsALinia = True
-            errorsLiniaActual.append("País en blanc a la columna 3")
-            register_error(line_counter, "País en blanc a la columna 3", problemes)
+            errorsLiniaActual.append("Aquatic en blanc a la columna 3")
+            register_error(line_counter, "Aquatic en blanc a la columna 3", problemes)
         else:
-            p = get_model_by_attribute('nom', line[2].strip(), Pais)
-            if p is None:
-                errorsALinia = True
-                errorsLiniaActual.append("No s'ha trobat el país '" + line[2] + "' a la columna 3")
-                register_error(line_counter, "No s'ha trobat el país '" + line[2] + "' a la columna 3", problemes)
-
-        if line[3] == '' or line[3].strip() == '':
-            errorsALinia = True
-            errorsLiniaActual.append("Aquatic en blanc a la columna 4")
-            register_error(line_counter, "Aquatic en blanc a la columna 4", problemes)
-        else:
-            if line[3].strip().lower() in ['true', 'cert', 'sí', 'si', '1', 'cierto', 'verdadero']:
+            if line[2].strip().lower() in ['true', 'cert', 'sí', 'si', '1', 'cierto', 'verdadero']:
                 aquatic = 'S'
 
-        if line[4] == '' or line[4].strip() == "":
+        if line[3] == '' or line[3].strip() == "":
             pare = None
         else:
-            ts = get_toponim_nom_estructurat(line[4])
+            ts = get_toponim_nom_estructurat(line[3])
             if len(ts) == 0:
                 errorsALinia = True
-                errorsLiniaActual.append("No trobo cap topònim node superior amb nom '" + line[4] + "' a la columna 5")
-                register_error(line_counter, "No trobo cap topònim node superior amb nom '" + line[4] + "' a la columna 5", problemes)
+                errorsLiniaActual.append("No trobo cap topònim node superior amb nom '" + line[3] + "' a la columna 4")
+                register_error(line_counter, "No trobo cap topònim node superior amb nom '" + line[3] + "' a la columna 4", problemes)
             elif len(ts) == 1:
                 pare = ts[0]
             else:
                 errorsALinia = True
-                errorsLiniaActual.append("Hi ha múltiples topònims  node superior amb nom '" + line[4] + "' a la columna 5")
-                register_error(line_counter, "Hi ha múltiples topònims  node superior amb nom '" + line[4] + "' a la columna 5", problemes)
+                errorsLiniaActual.append("Hi ha múltiples topònims  node superior amb nom '" + line[3] + "' a la columna 4")
+                register_error(line_counter, "Hi ha múltiples topònims  node superior amb nom '" + line[3] + "' a la columna 4", problemes)
 
-        if line[5] == '' or line[5].strip().lower() == '':
+        if line[4] == '' or line[4].strip().lower() == '':
             errorsALinia = True
-            errorsLiniaActual.append("Número de versió en blanc a la columna 6")
-            register_error(line_counter, "Número de versió en blanc a la columna 6", problemes)
+            errorsLiniaActual.append("Número de versió en blanc a la columna 5")
+            register_error(line_counter, "Número de versió en blanc a la columna 5", problemes)
         else:
             try:
-                numeroVersio = int(line[5])
+                numeroVersio = int(line[4])
             except ValueError:
                 errorsALinia = True
-                errorsLiniaActual.append("No sé convertir en nombre '" + line[5] + "' a la columna 6")
-                register_error(line_counter, "No sé convertir en nombre '" + line[5] + "' a la columna 6", problemes)
+                errorsLiniaActual.append("No sé convertir en nombre '" + line[4] + "' a la columna 5")
+                register_error(line_counter, "No sé convertir en nombre '" + line[4] + "' a la columna 5", problemes)
 
-        if line[6] is None or line[6].strip().lower() == '':
+        if line[5] is None or line[5].strip().lower() == '':
             qv = None # No és obligatori
         else:
-            qv = get_model_by_attribute('qualificador', line[6].strip(), Qualificadorversio)
+            qv = get_model_by_attribute('qualificador', line[5].strip(), Qualificadorversio)
             if qv is None:
                 errorsALinia = True
-                errorsLiniaActual.append("No trobo el qualificador de versió '" + line[6] + "' a la columna 7")
-                register_error(line_counter, "No trobo el qualificador de versió '" + line[6] + "' a la columna 7", problemes)
+                errorsLiniaActual.append("No trobo el qualificador de versió '" + line[5] + "' a la columna 6")
+                register_error(line_counter, "No trobo el qualificador de versió '" + line[5] + "' a la columna 6", problemes)
 
-        if line[7] is None or line[7].strip().lower() == '':
+        if line[6] is None or line[6].strip().lower() == '':
             errorsALinia = True
-            errorsLiniaActual.append("El recurs de georeferenciacio en que es basa el recurs està en blanc a la columna 8")
-            register_error(line_counter, "El recurs de georeferenciacio en que es basa el recurs està en blanc a la columna 8", problemes)
+            errorsLiniaActual.append("El recurs de georeferenciacio en que es basa el recurs està en blanc a la columna 7")
+            register_error(line_counter, "El recurs de georeferenciacio en que es basa el recurs està en blanc a la columna 7", problemes)
         else:
-            rg = get_model_by_attribute('nom', line[7].strip(), Recursgeoref)
+            rg = get_model_by_attribute('nom', line[6].strip(), Recursgeoref)
             if rg is None:
                 errorsALinia = True
-                errorsLiniaActual.append("No trobo el recurs de georeferenciació '" + line[7] + "' a la columna 8")
-                register_error(line_counter, "No trobo el recurs de georeferenciació '" + line[7] + "' a la columna 8", problemes)
+                errorsLiniaActual.append("No trobo el recurs de georeferenciació '" + line[6] + "' a la columna 8")
+                register_error(line_counter, "No trobo el recurs de georeferenciació '" + line[6] + "' a la columna 8", problemes)
 
-        if line[8] is None or line[8].strip().lower() == '':
+        if line[7] is None or line[7].strip().lower() == '':
             errorsALinia = True
             errorsLiniaActual.append("El nom del topònim al recurs de georeferenciacio està en blanc a la columna 9")
             register_error(line_counter, "El nom del topònim al recurs de georeferenciacio està en blanc a la columna 9", problemes)
         else:
-            nomToponimRecurs = line[8]
+            nomToponimRecurs = line[7]
 
-        if line[9] is None or line[9].strip().lower() == '':
+        if line[8] is None or line[8].strip().lower() == '':
             data = None
         else:
             try:
-                data = datetime.strptime(line[9].strip(), '%d/%m/%Y')
+                data = datetime.strptime(line[8].strip(), '%d/%m/%Y')
             except ValueError:
                 errorsALinia = True
-                errorsLiniaActual.append("Error convertint " + line[9] + " a format data a  la columna 10")
-                register_error(line_counter, "Error convertint " + line[9] + " a format data a  la columna 10", problemes)
+                errorsLiniaActual.append("Error convertint " + line[8] + " a format data a  la columna 10")
+                register_error(line_counter, "Error convertint " + line[8] + " a format data a  la columna 10", problemes)
 
         #x coord
-        if line[10] is None or line[10].strip().lower() == '':
+        if line[9] is None or line[9].strip().lower() == '':
             errorsALinia = True
             errorsLiniaActual.append("La coordenada x original està en blanc a la columna 11")
             register_error(line_counter, "La coordenada x original està en blanc a la columna 11", problemes)
         else:
             coordX_original = None
             try:
-                float(line[10].strip().replace(",", "."))
-                coordX_original = line[10].strip().replace(",", ".")
+                float(line[9].strip().replace(",", "."))
+                coordX_original = line[9].strip().replace(",", ".")
             except ValueError:
                 errorsALinia = True
-                errorsLiniaActual.append("Error de conversió a la coordenada x original " + line[10] +  ", columna 11")
-                register_error(line_counter, "Error de conversió a la coordenada x original " + line[10] +  ", columna 11", problemes)
+                errorsLiniaActual.append("Error de conversió a la coordenada x original " + line[9] +  ", columna 11")
+                register_error(line_counter, "Error de conversió a la coordenada x original " + line[9] +  ", columna 11", problemes)
 
         # coord y
-        if line[11] is None or line[11].strip().lower() == '':
+        if line[10] is None or line[10].strip().lower() == '':
             errorsALinia = True
             errorsLiniaActual.append("La coordenada y original està en blanc a la columna 12")
             register_error(line_counter, "La coordenada y original està en blanc a la columna 12", problemes)
         else:
             try:
-                float(line[11].strip().replace(",", "."))
-                coordY_original = line[11].replace(",", ".")
+                float(line[10].strip().replace(",", "."))
+                coordY_original = line[10].replace(",", ".")
             except ValueError:
                 errorsALinia = True
-                errorsLiniaActual.append("Error de conversió a la coordenada y original " + line[11] +  ", columna 11")
-                register_error(line_counter, "Error de conversió a la coordenada y original " + line[11] +  ", columna 11", problemes)
+                errorsLiniaActual.append("Error de conversió a la coordenada y original " + line[10] +  ", columna 11")
+                register_error(line_counter, "Error de conversió a la coordenada y original " + line[10] +  ", columna 11", problemes)
 
         # coord z
-        if line[12] is None or line[12].strip().lower() == '':
+        if line[11] is None or line[11].strip().lower() == '':
             errorsALinia = True
             errorsLiniaActual.append("La coordenada z original està en blanc a la columna 13")
             register_error(line_counter, "La coordenada z original està en blanc a la columna 13", problemes)
         else:
             try:
-                float(line[12].replace(",", "."))
-                coordZ_original = line[12].replace(",", ".")
+                float(line[11].replace(",", "."))
+                coordZ_original = line[11].replace(",", ".")
             except ValueError:
                 errorsALinia = True
-                errorsLiniaActual.append("Error de conversió a la coordenada z original " + line[12] +  ", columna 13")
-                register_error(line_counter, "Error de conversió a la coordenada z original " + line[12] +  ", columna 13", problemes)
+                errorsLiniaActual.append("Error de conversió a la coordenada z original " + line[11] +  ", columna 13")
+                register_error(line_counter, "Error de conversió a la coordenada z original " + line[11] +  ", columna 13", problemes)
 
 
         # incertesa coordenades h
-        if line[13] is not None and not line[13].strip().lower() == '':
+        if line[12] is not None and not line[12].strip().lower() == '':
             try:
-                precisioH = float(line[13].strip().replace(",", "."))
+                precisioH = float(line[12].strip().replace(",", "."))
             except ValueError:
                 errorsALinia = True
-                errorsLiniaActual.append("Error de conversió a incertesa de coordenades " + line[13] +  ", columna 14")
-                register_error(line_counter, "Error de conversió a incertesa de coordenades " + line[13] +  ", columna 14", problemes)
+                errorsLiniaActual.append("Error de conversió a incertesa de coordenades " + line[12] +  ", columna 14")
+                register_error(line_counter, "Error de conversió a incertesa de coordenades " + line[12] +  ", columna 14", problemes)
 
         # incertesa coordenades z
-        if line[14] is None or line[14].strip().lower() == '':
+        if line[13] is None or line[13].strip().lower() == '':
             precisioZ = None
         else:
             try:
-                float(line[14].strip().replace(",", "."))
-                precisioZ = line[14]
+                float(line[13].strip().replace(",", "."))
+                precisioZ = line[13]
             except ValueError:
                 errorsALinia = True
-                errorsLiniaActual.append("Error de conversió a incertesa d'altitud " + line[14] +  ", columna 15")
-                register_error(line_counter, "Error de conversió a incertesa d'altitud " + line[14] +  ", columna 15", problemes)
+                errorsLiniaActual.append("Error de conversió a incertesa d'altitud " + line[13] +  ", columna 15")
+                register_error(line_counter, "Error de conversió a incertesa d'altitud " + line[13] +  ", columna 15", problemes)
 
         # georeferenciador versio
-        if line[15] is None or line[15].strip().lower() == '':
+        if line[14] is None or line[14].strip().lower() == '':
             errorsALinia = True
             errorsLiniaActual.append("Georeferenciador en blanc a la columna 16")
-            register_error(line_counter, "Georeferenciador en blanc a la columna 16", problemes)            
+            register_error(line_counter, "Georeferenciador en blanc a la columna 16", problemes)
         else:
-            georeferenciador = get_georeferencer_by_name_simple(line[15].strip())
+            georeferenciador = get_georeferencer_by_name_simple(line[14].strip())
             if georeferenciador is None:
                 errorsALinia = True
-                errorsLiniaActual.append("No trobo el georeferenciador " + line[15] +  ", columna 16")
-                register_error(line_counter, "No trobo el georeferenciador " + line[15] +  ", columna 16", problemes)
+                errorsLiniaActual.append("No trobo el georeferenciador " + line[14] +  ", columna 16")
+                register_error(line_counter, "No trobo el georeferenciador " + line[14] +  ", columna 16", problemes)
 
-        if len(line) > 16:
-            observacions = line[16]
+        if len(line) > 15:
+            observacions = line[15]
         else:
             observacions = ''
 
