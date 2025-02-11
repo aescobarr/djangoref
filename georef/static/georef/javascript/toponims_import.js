971a2cc5370b3e63525d74339e7473fa099f7d7d
aescobarr <agustiescobar@gmail.com>
2025-02-11 15:38:06 +0100

    wip

--- georef/static/georef/javascript/toponims_import.js
+++ georef/static/georef/javascript/toponims_import.js
@@ -10,54 +10,101 @@ $(document).ready(function() {
                     xhr.setRequestHeader('X-CSRFToken', csrftoken);
                 }
             },
-            success: function( data, textStatus, jqXHR ) {
-                //console.log(data.detail);
+            success: function( data, textStatus, jqXHR ) {                
                 processResponse(jqXHR.responseJSON);
                 $('.qq-upload-list').empty();
             },
-            error: function(jqXHR, textStatus, errorThrown){
-                //console.log(jqXHR.responseJSON);
-                //toastr.error(jqXHR.responseJSON.detail);
+            error: function(jqXHR, textStatus, errorThrown){                
                 processResponse(jqXHR.responseJSON);
             }
         });
     }
 
-    var uploader = new qq.FileUploader({
-        action: _ajax_upload_url,
-        element: $('#fileuploader')[0],
-        multiple: false,
-        onSubmit: function(id, fileName){
-            $('#filename').val('');
-            var regex = /^[\w.]{0,256}$/;
-            if( regex.exec(fileName) == null){
-                toastr.error(gettext('Error a nom de fitxer! Només s\'admeten lletres (majúscules i minúscules), números i el caràcter "_"'));
-                return false;
-            }
-            resetInterface();
-            this.params.deletePrevious = true;
-        },
-        onComplete: function(id, fileName, responseJSON) {
-            if(responseJSON.success) {
-                //$('#filename').val(responseJSON.filename);
-                //toastr.success('Fitxer carregat al servidor amb èxit!')
-                //console.log(responseJSON.detail);
-                import_csv(responseJSON.filename);
-            } else {
-                toastr.error('Error pujant fitxer!')
-            }
-        },
-        template:'<div class="qq-uploader">' +
-            '<div class="qq-upload-drop-area"><span>' + gettext('Pujar fitxer') + '</span></div>' +
-            '<div class="qq-upload-button ui-widget-content ui-button ui-corner-all ui-state-default">' + gettext('Pujar fitxer') + '</div>' +
-            '<ul class="qq-upload-list"></ul>' +
-            '</div>',
-        params: {
-            'csrf_token': csrf_token,
-            'csrf_name': 'csrfmiddlewaretoken',
-            'csrf_xname': 'X-CSRFToken',
+    var uploadFile = function () {
+        let fileInput = $('#fileInput')[0];
+    
+        if (fileInput.files.length === 0) {
+            alert("Please select a file.");
+            return;
         }
-    });
+    
+        let file = fileInput.files[0];
+        let formData = new FormData();
+        formData.append("file", file);        
+    
+        $.ajax({
+            url: _ajax_upload_url,  // Make sure this variable is defined
+            type: "POST",
+            data: formData,
+            processData: false,  // Prevent jQuery from processing the data
+            contentType: false,  // Prevent jQuery from setting content type
+            beforeSend: function(xhr, settings) {
+                if (!csrfSafeMethod(settings.type)) {
+                    var csrftoken = getCookie('csrftoken');
+                    xhr.setRequestHeader('X-CSRFToken', csrftoken);
+                }
+                resetInterface();
+            },            
+            xhr: function () {
+                let xhr = new window.XMLHttpRequest();
+                xhr.upload.addEventListener("progress", function (event) {
+                    if (event.lengthComputable) {
+                        let percent = (event.loaded / event.total) * 100;
+                        $("#progress-bar").css("width", percent + "%").text(Math.round(percent) + "%");
+                        $("#progress-container").show();
+                    }
+                }, false);
+                return xhr;
+            },
+            success: function (response) {
+                if (response.success) {
+                    $("#message").html(`<p>File uploaded successfully: <a href="${response.file_url}" target="_blank">${response.file_url}</a></p>`);                    
+                    import_csv(response.filename);
+                } else {
+                    $("#message").html("<p>Upload failed!</p>");
+                }
+            },
+            error: function () {
+                $("#message").html("<p>An error occurred while uploading.</p>");
+            }
+        });
+    };
+
+    // var uploader = new qq.FileUploader({
+    //     action: _ajax_upload_url,
+    //     element: $('#fileuploader')[0],
+    //     multiple: false,
+    //     onSubmit: function(id, fileName){
+    //         $('#filename').val('');
+    //         var regex = /^[\w.]{0,256}$/;
+    //         if( regex.exec(fileName) == null){
+    //             toastr.error(gettext('Error a nom de fitxer! Només s\'admeten lletres (majúscules i minúscules), números i el caràcter "_"'));
+    //             return false;
+    //         }
+    //         resetInterface();
+    //         this.params.deletePrevious = true;
+    //     },
+    //     onComplete: function(id, fileName, responseJSON) {
+    //         if(responseJSON.success) {
+    //             //$('#filename').val(responseJSON.filename);
+    //             //toastr.success('Fitxer carregat al servidor amb èxit!')
+    //             //console.log(responseJSON.detail);
+    //             import_csv(responseJSON.filename);
+    //         } else {
+    //             toastr.error('Error pujant fitxer!')
+    //         }
+    //     },
+    //     template:'<div class="qq-uploader">' +
+    //         '<div class="qq-upload-drop-area"><span>' + gettext('Pujar fitxer') + '</span></div>' +
+    //         '<div class="qq-upload-button ui-widget-content ui-button ui-corner-all ui-state-default">' + gettext('Pujar fitxer') + '</div>' +
+    //         '<ul class="qq-upload-list"></ul>' +
+    //         '</div>',
+    //     params: {
+    //         'csrf_token': csrf_token,
+    //         'csrf_name': 'csrfmiddlewaretoken',
+    //         'csrf_xname': 'X-CSRFToken',
+    //     }
+    // });
 
     $('#details').click(function(){
         //$('.persiana').toggle();
@@ -79,6 +126,13 @@ $(document).ready(function() {
 
     var resetInterface = function(){
         $("#errors").hide();
+        $('#sumari thead').empty();
+        $('#sumari tbody').empty();
+        $('#creats thead').empty();
+        $('#creats tbody').empty();
+        $('#existents thead').empty();
+        $('#existents tbody').empty();
+        $('#linkDescarrega').empty();
     }
 
     var mostrarCaixaErrors = function(text){
@@ -181,5 +235,7 @@ $(document).ready(function() {
         $("a#linkDescarrega").text( gettext('Descarrega fitxer csv de resum') );
     };
 
-
+    $('#fileInput').on('change',function(e){
+        uploadFile();        
+    });
 });
